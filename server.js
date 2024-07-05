5b809cb01af1d9585685b5d53ed4ba0fbb40c3e7
Nataly Maria <natalymariamaria4@gmail.com>
2024-07-05 13:59:36 -0300

    Rota GET /receitas finalizadas

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 import {createServer} from 'node:http';
 import fs from "node:fs";
 
+import lerDadosReceitas from "./helper/lerReceitas.js"
+
 const PORT = 3333;
 
 const server = createServer((request, response)=>{
     const {url, method} = request;
-
+    //#152 - adicionar a rota GET receitas com essas
     if(method === 'GET' && url === "/receitas"){
-
+        lerDadosReceitas((err, receitas)=>{
+            if(err){
+                response.writeHead(500, {"Content-Type": "application/json"})
+                response.end(JSON.stringify({message: "Erro ao ler os dados"}))
+                return
+            }
+            //imprimir o resultado
+            response.writeHead(200, {"Content-Type": "application/json"})
+            response.end(JSON.stringify(receitas))
+        })
     }else if(method === 'POST' && url === '/receitas'){
 
     }else if(method === 'GET' && url.startsWith('/receitas/')){
