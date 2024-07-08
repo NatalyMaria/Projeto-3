275a159c67865fa6d4420f858b04d37a224e064b
Nataly Maria <natalymariamaria4@gmail.com>
2024-07-08 14:19:51 -0300

    Rota cdastro de receitas e ADD UUID

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import {createServer} from 'node:http';
 import fs from "node:fs";
+import { v4 as uuidv4 } from 'uuid';
 
-import lerDadosReceitas from "./helper/lerReceitas.js"
+import lerDadosReceita from './helper/lerReceitas.js';
 
 const PORT = 3333;
 
@@ -20,7 +21,33 @@ const server = createServer((request, response)=>{
             response.end(JSON.stringify(receitas))
         })
     }else if(method === 'POST' && url === '/receitas'){
+        let body = ""
+        request.on("data", (chunk)=>{
+            body += chunk;
+        })
+        request.on('end', ()=>{
+            const novaReceita = JSON.parse(body)
+            lerDadosReceita((err, receitas)=>{
+                if(err){
+                    response.writeHead(500, {"Content-Type": "application/json"})
+                    response.end(JSON.stringify({message: "Erro ao ler receitas"}))
+                    return
+                }
+                //[1,2,3,4,5,6,7,90]
+                novaReceita.id = uuidv4();
+                receitas.push(novaReceita);
 
+                fs.writeFile("receitas.json", JSON.stringify(receitas, null, 2), ()=>{
+                    if(err){
+                        response.writeHead(500, {"Content-Type": "application/json"})
+                        response.end(JSON.stringify({message: "Erro ao cadastrar receitas"}))
+                        return
+                    }
+                    response.writeHead(201, {"Content-Type": "application/json"})
+                    response.end(JSON.stringify({novaReceita}))
+                })
+            })
+        })
     }else if(method === 'GET' && url.startsWith('/receitas/')){
         
     }else if(method === 'PUT' && url.startsWith('/receitas/')){
